2ca93a81fb6e318a4e3423d4e02b9690274c508c
makeworld <makeworld@protonmail.com>
2019-02-09 12:26:19 -0500

    Set PID and removed not used cargo vars

--- src/main/java/robot/RobotConst.java
+++ src/main/java/robot/RobotConst.java
@@ -29,16 +29,6 @@ public class RobotConst {
     public static final double  ULTRASONIC_VOLTAGE_40IN       = 0.383;
     public static final double  ULTRASONIC_VOLTAGE_80IN       = 0.764;
 
-    // For CargoSubsystem
-    public static final double  CARGO_GATE_ENCODER_COUNTS_PER_INCH;  // Unused
-    public static final double  CARGO_GATE_SPEED_PID_KP;
-    public static final double  CARGO_GATE_SPEED_PID_KI;
-    public static final double  CARGO_GATE_MAX_LOW_GEAR_SPEED;
-    // Speed to turn the motors when opening the gate
-    public static final double  CARGO_GATE_MOTOR_SPEED;
-    // Encoder distance for the motors to turn enough to open/close the gate
-    public static final double  CARGO_GATE_TOGGLE_ENCODER_DISTANCE;
-
     // Operator speed limitation - as a division of the motor speed
     // XXX: Needs to be changed after testing
     public static final double OPERATOR_SPEED_DIVISOR = 4;
@@ -70,18 +60,11 @@ public class RobotConst {
             DRIVE_GYRO_PID_KP = .01;
             DRIVE_GYRO_PID_KI = DRIVE_GYRO_PID_KP / 20.0;
 
-            DRIVE_SPEED_PID_KP = 1;
-            DRIVE_SPEED_PID_KI = DRIVE_SPEED_PID_KP / 20.0;
- 
-            ENCODER_COUNTS_PER_INCH = 28.52;
+            DRIVE_SPEED_PID_KP = 0.0045;
+            //DRIVE_SPEED_PID_KI = DRIVE_SPEED_PID_KP / 20.0;
+            DRIVE_SPEED_PID_KI = 0.001;
 
-            // CargoSubsystem
-            CARGO_GATE_ENCODER_COUNTS_PER_INCH = 0;  // Unused
-            CARGO_GATE_SPEED_PID_KP = 0;
-            CARGO_GATE_SPEED_PID_KI = 0;
-            CARGO_GATE_MAX_LOW_GEAR_SPEED = 0;  // XXX: Needs to be set
-            CARGO_GATE_MOTOR_SPEED = 0;
-            CARGO_GATE_TOGGLE_ENCODER_DISTANCE = 0;
+            ENCODER_COUNTS_PER_INCH = 28.52;
 
             break;
         }
