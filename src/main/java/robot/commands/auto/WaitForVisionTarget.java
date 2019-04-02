8b7ae0e5d073e21dede7a33fd232f43cf567aa84
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-04-01 23:13:36 -0400

    Add parameters to control timeouts during auto patterns

--- src/main/java/robot/commands/auto/WaitForVisionTarget.java
+++ src/main/java/robot/commands/auto/WaitForVisionTarget.java
@@ -87,8 +87,11 @@ public class WaitForVisionTarget extends TSafeCommand {
                 logMessage("Schedule auto align to heading " + heading
                 		+ " and drive distance " + distanceInches);
 
+                double visionTimeout = 2; // Allow 2 seconds for vision targeting
+                double driveTimeout = 3; // Allow 3 seconds for distance timeout
+                
     	        Scheduler.getInstance().add(new AutoAlignAndDeliver(
-    	                heading, distanceInches, 0.25));
+    	                heading, distanceInches, 0.25, visionTimeout, driveTimeout));
     	    }
     	    return true;
     	}
